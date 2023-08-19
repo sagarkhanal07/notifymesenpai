3a9409715325cc5f94570e4765dc7b3247ec035b
sagarkhanal07 <sagar@optimumfuturist.com>
2023-08-19 08:09:46 +0545

    Empty commit

--- scripts/checkAvailability.js
+++ scripts/checkAvailability.js
@@ -1,4 +1,8 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const screenshotPath = "../images/screenshot.png";
+const readmePath = "../README.md";
 
 async function checkButtonStatus(website, selector) {
     const browser = await puppeteer.launch({ headless: true });
@@ -13,9 +17,13 @@ async function checkButtonStatus(website, selector) {
 
         if (isDisabled) {
             console.log('Button is disabled.');
+            await button.screenshot({ path: screenshotPath });
+            updateReadme(readmePath, 'Button is disabled.', screenshotPath);
             process.exit(0);
         } else {
             console.log('Button is enabled.');
+            await button.screenshot({ path: screenshotPath });
+            updateReadme(readmePath, 'Button is enabled.', screenshotPath);
             process.exit(1); // Different exit code for enabled
         }
     } catch (error) {
@@ -26,4 +34,9 @@ async function checkButtonStatus(website, selector) {
     }
 }
 
-module.exports = { checkButtonStatus }
+function updateReadme(readmePath, status, screenshotPath) {
+    const statusText = `Status:\n${status}\n![Button Screenshot](${screenshotPath})`;
+    fs.writeFileSync(readmePath, statusText, { encoding: 'utf-8' });
+}
+
+module.exports = { checkButtonStatus };
